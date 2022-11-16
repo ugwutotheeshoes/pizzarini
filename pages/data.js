f3002ca86c3f6500ca1003acffcafb3736f786fa
Emmanuel Ugwu <>
2022-11-16 20:00:35 +0100

    New changes

---
+++ pages/data.js
@@ -0,0 +1,36 @@
+const menu = [
+  {
+    id: 1,
+    title: "chicken triple-decker",
+    category: "breakfast",
+    price: 15.99,
+    img: "https://res.cloudinary.com/ugwutotheeshoes/image/upload/c_scale,h_1072,w_1712/v1668386101/pizzarini/mexican-pizza.jpg",
+    desc: `Three layers, three cheeses, nothing more, nothing less. Our super super secret cream cheese, chicken, diced tomato, mushroom, onion, tomato & herb pizza sauce & mozzarella, topped with Luigi sauce, cheddar & mozzarella.`,
+  },
+  {
+    id: 2,
+    title: "meat triple-decker",
+    category: "lunch",
+    price: 12.99,
+    img: "https://res.cloudinary.com/ugwutotheeshoes/image/upload/c_scale,h_1072,w_1712/v1668386112/pizzarini/sharwarma-pizza.jpg",
+    desc: `Three layers, three cheeses. not two, not four, three! Our super super secret cream cheese, ground beef, beef spare rib, bacon, onion, tomato & herb pizza sauce & mozzarella, topped with BBQ sauce, cheddar & mozzarella.`,
+  },
+  {
+    id: 3,
+    title: "chicken double-stack",
+    category: "shakes",
+    price: 20.00,
+    img: "https://res.cloudinary.com/ugwutotheeshoes/image/upload/c_scale,h_1072,w_1712/v1668386096/pizzarini/triple-stack.jpg",
+    desc: `Our secret cream cheese filling sandwiched between two crisp bases & topped with tomato & herb pizza sauce, chicken, mushroom, onion, diced tomato, tikka sauce, cheddar, mozzarella & spring onion.`,
+  },
+  {
+    id: 4,
+    title: "meat sharwarma",
+    category: "breakfast",
+    price: 11.99,
+    img: "https://res.cloudinary.com/ugwutotheeshoes/image/upload/v1668386096/pizzarini/triple-stack.jpg",
+    desc: `Luigi Special Sauce, 5Sausages, Ground Beef, Mozzarella, Cabbage, Carrot, Onion, Scent Pepper, Suya Spice, Pizza Sauce.`,
+  },
+];
+
+export default menu;
