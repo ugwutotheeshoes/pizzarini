a2159b4f01a82ca100fbc556771247893a470ae9
Emmanuel Ugwu <>
2022-11-16 20:12:08 +0100

    Made changes

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,42 @@
 /* eslint-disable react/no-unescaped-entities */
-import Head from 'next/head'
-import Image from 'next/image'
 import React, { useState } from "react";
-import menu from "./data";
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 import { FaPizzaSlice } from "react-icons/fa";
 
-import { BaseClient } from '@xata.io/client';
+import { BaseClient } from "@xata.io/client";
 
 export async function getServerSideProps() {
   const xata = new BaseClient({
-    branch: 'main',
-    apiKey: process.env.API_KEY,
-    databaseURL: "https://Ugwu-s-workspace-mv4vl6.us-east-1.xata.sh/db/pizzarini"
+    branch: "main",
+    apiKey: "xau_KgAbIsJZRGkJiDxmgJTuoROXDIplVPoP1",
+    databaseURL:
+      "https://Ugwu-s-workspace-mv4vl6.us-east-1.xata.sh/db/pizzarini",
   });
-  
-   const records = await xata.db["pizza-menu"].getAll();
-  console.log(records)
 
-return {
-  props: {
-    pizzas: records 
-  }
-}
+  const records = await xata.db["pizza-menu"].getAll();
+  console.log(records);
+
+  return {
+    props: {
+      pizzas: records,
+    },
+  };
 }
 
-export default function Home({pizzas}) {
+export default function Home({ pizzas }) {
   const [menuItems, setMenuItems] = useState(menu);
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = (e) => {
     e.preventDefault();
     setIsOpen(true);
-    // update();
   };
   const trigger = (e) => {
     e.preventDefault();
     setIsOpen(false);
   };
   return (
-<div className={styles.App}>
+    <div className={styles.App}>
       <div className={styles.icon}>
         <div className={styles.logo}>
           <h2>Luigi's Pizza</h2>
@@ -47,19 +44,19 @@ export default function Home({pizzas}) {
         </div>
         {isOpen ? <button onClick={trigger}>go back</button> : ""}
       </div>
-      {/* <ChickenStack /> */}
       {!isOpen ? (
         <div
           className={styles.toggle}
           style={isOpen ? { marginTop: 0 } : { marginTop: 280 }}
         >
-          <p className={styles.itemText}>Step into heaven in the form of a pizza slice.</p>
+          <p className={styles.itemText}>
+            Step into heaven in the form of a pizza slice.
+          </p>
           <button onClick={toggle}>see pizza menu</button>
         </div>
       ) : (
         ""
       )}
-      {/* <Xata /> */}
       {isOpen ? (
         <div className={styles.container}>
           {pizzas.map((menuItem) => {
@@ -70,7 +67,6 @@ export default function Home({pizzas}) {
                 <div className={styles.itemInfo}>
                   <header>
                     <h4>{title}</h4>
-                    {/* <h4>{category}</h4> */}
                     <h4 className={styles.price}>${price}</h4>
                   </header>
                   <p className={styles.itemText}>{desc}</p>
@@ -82,9 +78,7 @@ export default function Home({pizzas}) {
       ) : (
         ""
       )}
-      <footer className={styles.footer}>
-      Luigi's Pizza | 2022
-      </footer>
+      <footer className={styles.footer}>Luigi's Pizza | 2022</footer>
     </div>
-  )
+  );
 }
